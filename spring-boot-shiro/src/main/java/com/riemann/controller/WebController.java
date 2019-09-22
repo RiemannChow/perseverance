f72f7503e783270060747fcd2a7a94377c7dbe72
riemann <754798352@qq.com>
2019-09-23 00:10:50 +0800

    WebController

---
+++ spring-boot-shiro/src/main/java/com/riemann/controller/WebController.java
@@ -0,0 +1,74 @@
+package com.riemann.controller;
+
+import com.riemann.bean.ResponseBean;
+import com.riemann.bean.UserBean;
+import com.riemann.exception.UnauthorizedException;
+import com.riemann.service.UserService;
+import com.riemann.util.JWTUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WebController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebController.class);
+
+    private UserService userService;
+
+    @Autowired
+    public void setService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public ResponseBean login(String username, String password) {
+        UserBean userBean = userService.getUser(username);
+        if (userBean.getPassword().equals(password)) {
+            return new ResponseBean(200, "Login success", JWTUtil.sign(username, password));
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+    @GetMapping("/article")
+    public ResponseBean article() {
+        Subject subject = SecurityUtils.getSubject();
+        if (subject.isAuthenticated()) {
+            return new ResponseBean(200, "You are already logged in", null);
+        } else {
+            return new ResponseBean(200, "You are guest", null);
+        }
+    }
+
+    @GetMapping("/require_auth")
+    @RequiresAuthentication
+    public ResponseBean requireAuth() {
+        return new ResponseBean(200, "You are authenticated", null);
+    }
+
+    @GetMapping("/require_role")
+    @RequiresRoles("admin")
+    public ResponseBean requireRole() {
+        return new ResponseBean(200, "You are visiting require_role", null);
+    }
+
+    @GetMapping("/require_permission")
+    @RequiresPermissions(logical = Logical.AND, value = {"view", "edit"})
+    public ResponseBean requirePermission() {
+        return new ResponseBean(200, "You are visiting permission require edit,view", null);
+    }
+
+    @RequestMapping(path = "/401")
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseBean unauthorized() {
+        return new ResponseBean(401, "Unauthorized", null);
+    }
+}
