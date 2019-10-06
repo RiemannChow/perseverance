55130083bae58741ec20a98c640516fe2f6ecb98
riemann <754798352@qq.com>
2019-10-06 14:02:20 +0800

    microservice-eureka-ribbon

---
+++ spring-cloud-example/microservice-eureka-ribbon/microservice-eureka-ribbon-server/src/main/java/com/riemann/microserviceeurekaserver/MicroserviceEurekaRibbonServerApplication.java
@@ -0,0 +1,15 @@
+package com.riemann.microserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceEurekaRibbonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEurekaRibbonServerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-example/microservice-eureka-ribbon/microservice-provider-service-ribbon/src/main/java/com/riemann/microserviceproviderservicehi/HelloControler.java
@@ -0,0 +1,19 @@
+package com.riemann.microserviceproviderservicehi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+
+}
