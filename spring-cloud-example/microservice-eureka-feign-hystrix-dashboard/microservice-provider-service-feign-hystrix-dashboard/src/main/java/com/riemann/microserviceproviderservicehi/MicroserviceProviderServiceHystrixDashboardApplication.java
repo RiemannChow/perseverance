b59fd7240fe5308c0195c72374c5ee869e479726
riemann <754798352@qq.com>
2019-10-14 23:52:23 +0800

    microservice-eureka-feign-hystrix-dashboard

---
+++ spring-cloud-example/microservice-eureka-feign-hystrix-dashboard/microservice-provider-service-feign-hystrix-dashboard/src/main/java/com/riemann/microserviceproviderservicehi/MicroserviceProviderServiceHystrixDashboardApplication.java
@@ -0,0 +1,31 @@
+package com.riemann.microserviceproviderservicehi;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrixDashboard
+public class MicroserviceProviderServiceHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProviderServiceHystrixDashboardApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean hystrixMetricsStreamServlet() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+		registration.addUrlMappings("/hystrix.stream");
+		return registration;
+	}
+
+}
