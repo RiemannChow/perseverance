ab8d04bca5547fc074735b7ed7746644c6c46379
riemann <754798352@qq.com>
2019-10-17 23:54:25 +0800

    microservice-eureka-hystrix-turbine

---
+++ spring-cloud-example/microservice-eureka-hystrix-turbine/microservice-provider-service-hystrix-turbine/src/main/java/com/riemann/microserviceproviderservicehi/MicroserviceProviderServiceHystrixTurbineApplication.java
@@ -0,0 +1,40 @@
+package com.riemann.microserviceproviderservicehi;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
+public class MicroserviceProviderServiceHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProviderServiceHystrixTurbineApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
+
+}
