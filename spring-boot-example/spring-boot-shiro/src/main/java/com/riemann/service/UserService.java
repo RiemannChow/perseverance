42ac633b833d309a6318a60cbbf26c9bdc2d3b95
riemann <754798352@qq.com>
2020-03-02 12:05:45 +0800

    spring-boot-shiro

---
+++ spring-boot-example/spring-boot-shiro/src/main/java/com/riemann/service/UserService.java
@@ -0,0 +1,25 @@
+package com.riemann.service;
+
+import com.riemann.bean.UserBean;
+import com.riemann.database.DataSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class UserService {
+    public UserBean getUser(String username) {
+        // 没有此用户直接返回null
+        if (! DataSource.getData().containsKey(username))
+            return null;
+
+        UserBean user = new UserBean();
+        Map<String, String> detail = DataSource.getData().get(username);
+
+        user.setUsername(username);
+        user.setPassword(detail.get("password"));
+        user.setRole(detail.get("role"));
+        user.setPermission(detail.get("permission"));
+        return user;
+    }
+}
