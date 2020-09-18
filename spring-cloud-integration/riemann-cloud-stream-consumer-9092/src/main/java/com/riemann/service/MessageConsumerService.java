a9a47acacd8ed6a245115571d4eeae807c0b4fd1
riemann <754798352@qq.com>
2020-09-19 00:15:28 +0800

    riemann-cloud-stream-consumer-9092

---
+++ spring-cloud-integration/riemann-cloud-stream-consumer-9092/src/main/java/com/riemann/service/MessageConsumerService.java
@@ -0,0 +1,16 @@
+package com.riemann.service;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+
+@EnableBinding(Sink.class)
+public class MessageConsumerService {
+
+    @StreamListener(Sink.INPUT)
+    public void recevieMessages(Message<String> message) {
+        System.out.println("=========接收到的消息：" + message);
+    }
+
+}
