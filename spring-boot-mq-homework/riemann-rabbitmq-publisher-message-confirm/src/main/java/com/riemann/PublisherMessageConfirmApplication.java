779bcd25c8719eb8dcb92c4a9e7620e62de8874c
riemann <754798352@qq.com>
2021-02-17 10:07:54 +0800

    PublisherMessageConfirmApplication

---
+++ spring-boot-mq-homework/riemann-rabbitmq-publisher-message-confirm/src/main/java/com/riemann/PublisherMessageConfirmApplication.java
@@ -0,0 +1,11 @@
+package com.riemann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherMessageConfirmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PublisherMessageConfirmApplication.class, args);
+    }
+}
